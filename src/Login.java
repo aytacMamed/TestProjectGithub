d84b568efcb7cb19381013e78bdb1aaeb396963d
Aytac Memmedli <Aytac-Mammadli@gmail.com>
2024-11-13 19:06:51 +0300

    login

---
+++ src/Login.java
@@ -0,0 +1,5 @@
+public class Login {
+    public void login(String username,String password){
+        System.out.println(username+" adli ve "+password+" olan istifadeci sisteme ugurla daxil oldu");
+    }
+}
